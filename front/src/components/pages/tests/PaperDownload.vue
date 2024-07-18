dedc6ca168ef8a06ba3d584f178a4fe65c4ab622
FlashLizard <1256854232@qq.com>
2024-07-19 01:38:40 +0800

    feat: fix front for deploy3

--- front/src/components/pages/tests/PaperDownload.vue
+++ front/src/components/pages/tests/PaperDownload.vue
@@ -454,7 +454,7 @@ export default {
           this.loading = true;
 
           // 发送 POST 请求到后端获取试题文本
-          const response = await axios.post('http://localhost:5000/get_downloadproblems', formData, {
+          const response = await axios.post('/get_downloadproblems', formData, {
             responseType: 'blob' // 响应类型为 Blob
           });
 
